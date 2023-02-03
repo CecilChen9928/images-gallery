96ab6e96a3498973935f4e57cc177a20c5f1118b
CecilChen9928 <cecil090727@gmail.com>
2023-02-02 21:46:18 -0800

    edit search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+
 function App() {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+  }
+
+  console.log(word);
+
   return (
     <div>
       <Header title="Images of Gallery"/>
-      <Search />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Button, FormControl, Navbar } from 'react-bootstrap'
+import { Button, FormControl, Navbar, Container } from 'react-bootstrap'
+
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+}
 
 const Header = ({title}) => {
     return (
-        <Navbar bg="light" variant="light">
-            <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Navbar style={navbarStyle} variant="light">
+            <Container>
+                <Navbar.Brand href="/">{title}</Navbar.Brand>
+            </Container>
         </Navbar>
     )
 };
